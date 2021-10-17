b8c96fd62715610da7d255f641cdd7f6ec8cb6f5
GazelleMag <joao_xxx@hotmail.com>
2021-10-17 23:31:46 +0100

    started adding animation transitions to player game object

--- PuntsPats/Assets/Scripts/PlayerMovement.cs
+++ PuntsPats/Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 5f;
-    public Rigidbody2D playerRb;
-    public Camera camera;
-    Vector2 movement;
-    Vector2 mousePos;
+  public float movementSpeed = 5f;
+  public Rigidbody2D playerRb;
+  public Camera camera;
+  Vector2 movement;
+  Vector2 mousePos;
+  public Animator animator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+  // Update is called once per frame
+  void Update()
+  {
+    movement.x = Input.GetAxisRaw("Horizontal");
+    movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
-    }
+		IdleRunAnimTransition(movement.x, movement.y);
 
-    void FixedUpdate() 
-    {
-        playerRb.MovePosition(playerRb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+  }
 
-        Vector2 lookDirection = mousePos - playerRb.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-        playerRb.rotation = angle;
-    }
+  void FixedUpdate()
+  {
+    playerRb.MovePosition(playerRb.position + movement * movementSpeed * Time.fixedDeltaTime);
+
+    Vector2 lookDirection = mousePos - playerRb.position;
+    float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+    playerRb.rotation = angle;
+  }
+
+  void IdleRunAnimTransition(float movementX, float movementY)
+  {
+			if (movementX != 0 || movementY != 0)
+			{
+				animator.SetBool("Moving", true);
+			}
+			else
+			{
+				animator.SetBool("Moving", false);
+			}
+  }
 }
