07fcb89e3360f2d86ac0340e0b8958deec38596d
GazelleMag <joao_xxx@hotmail.com>
2021-10-17 21:56:48 +0100

    bullet game object now gets destroyed after some time

--- PuntsPats/Assets/Scripts/Bullet.cs
+++ PuntsPats/Assets/Scripts/Bullet.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect; //-> set an effect for the bullet
-    void OnCollisionEnter2D(Collision2D collision) 
+  public float destroyTime = 3.5f;
+  public GameObject hitEffect; //-> set an effect for the bullet
+
+  void Update()
+  {
+    if (gameObject)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 2.5f);
-        Destroy(gameObject);
+      Destroy(gameObject, destroyTime);
     }
+  }
+
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+    Destroy(effect, 2.5f);
+    Destroy(gameObject);
+  }
 }
